6f9927ad31cc8f4ba96e06697cd721a16419e0f2
MarcoChavezLoli <u201917180@upc.edu.pe>
2025-09-12 12:07:55 -0500

    Creacion tabla Distrito Comercial

---
+++ src/main/java/pe/edu/pe/bestprice/servicesimplements/LineaTiendasServiceImplement.java
@@ -0,0 +1,47 @@
+package pe.edu.pe.bestprice.servicesimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.pe.bestprice.entities.LineaTiendas;
+import pe.edu.pe.bestprice.repositories.ILineaTiendasRepository;
+import pe.edu.pe.bestprice.servicesinterfaces.ILineaTiendasService;
+
+import java.util.List;
+@Service
+public class LineaTiendasServiceImplement implements ILineaTiendasService {
+
+    @Autowired
+    private ILineaTiendasRepository repository;
+
+    @Override
+    public List<LineaTiendas>list(){
+        return repository.findAll();
+    }
+
+    @Override
+    public void insert(LineaTiendas lt) {
+        repository.save(lt);
+    }
+
+    @Override
+    public LineaTiendas ListId(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void update(LineaTiendas lt) {
+        repository.save(lt);
+    }
+
+    @Override
+    public List<LineaTiendas> buscarService(String nombre) {
+        return repository.buscar(nombre);
+    }
+
+
+}
