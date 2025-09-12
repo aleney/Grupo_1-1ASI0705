70430982e909aa6c5a0a00fc9da056b2084e4507
MarcoChavezLoli <u201917180@upc.edu.pe>
2025-09-12 12:07:55 -0500

    Creacion tabla Distrito Comercial

---
+++ src/main/java/pe/edu/pe/bestprice/entities/DistritosComerciales.java
@@ -0,0 +1,9 @@
+package pe.edu.pe.bestprice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="DComerciales")
+public class DistritosComerciales {
+}
