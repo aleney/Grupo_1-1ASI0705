a1339be52bddcdf63ad41652a43f48789f9ad520
MarcoChavezLoli <u201917180@upc.edu.pe>
2025-09-12 12:30:39 -0500

    PRUEBA GIT DISTRITO COMERCIAL

--- src/main/java/pe/edu/pe/bestprice/entities/DistritosComerciales.java
+++ src/main/java/pe/edu/pe/bestprice/entities/DistritosComerciales.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="DistritoComer")
+@Table(name="DistritoComercial")
 public class DistritosComerciales {
 
 }
