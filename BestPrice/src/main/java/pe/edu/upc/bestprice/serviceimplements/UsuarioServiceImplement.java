7548e1ecacb9191e06b68e1d4e68e4095e148bbf
Valo21-2024 <legueralvaro14@gmail.com>
2025-11-06 21:00:55 -0500

    Add method to list inactive users

--- BestPrice/src/main/java/pe/edu/upc/bestprice/serviceimplements/UsuarioServiceImplement.java
+++ BestPrice/src/main/java/pe/edu/upc/bestprice/serviceimplements/UsuarioServiceImplement.java
@@ -42,7 +42,9 @@ public class UsuarioServiceImplement implements IUsuarioService {
     }
 
     @Override
-    public List<String[]> listar(){ return repository.listar();};
+    public List<String[]> listarUsuariosInactivos(String estado) {
+        return repository.listarUsuariosInactivos(estado);
+    }
 
 
 }
