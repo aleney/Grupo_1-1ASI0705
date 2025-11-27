a41102f3a9507f0d1f35c8e645cc56a7552e88f8
Valo21-2024 <legueralvaro14@gmail.com>
2025-11-27 03:10:44 -0500

    corrigiendo TicketRespuesta

--- BestPrice/src/main/java/pe/edu/upc/bestprice/dtos/TicketRespuestaDTOInsert.java
+++ BestPrice/src/main/java/pe/edu/upc/bestprice/dtos/TicketRespuestaDTOInsert.java
@@ -1,11 +1,19 @@
 package pe.edu.upc.bestprice.dtos;
 
+import pe.edu.upc.bestprice.entities.TicketReporte;
+
 import java.time.LocalDateTime;
 
 public class TicketRespuestaDTOInsert {
     public int idTicketRes;
 
     public String detalle;
+
+    public LocalDateTime createdAt;
+
+    public int ticketReporte;
+
+    public int usuario;
 
     public int getIdTicketRes() {
         return idTicketRes;
@@ -23,4 +31,27 @@ public class TicketRespuestaDTOInsert {
         this.detalle = detalle;
     }
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getTicketReporte() {
+        return ticketReporte;
+    }
+
+    public void setTicketReporte(int ticketReporte) {
+        this.ticketReporte = ticketReporte;
+    }
+
+    public int getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(int usuario) {
+        this.usuario = usuario;
+    }
 }
---
+++ BestPrice/src/main/java/pe/edu/upc/bestprice/dtos/TicketRespuestaDTOList.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.bestprice.dtos;
+
+public class TicketRespuestaDTOList {
+
+    public int idTicketRes;
+
+    public String detalle;
+
+    public int getIdTicketRes() {
+        return idTicketRes;
+    }
+
+    public void setIdTicketRes(int idTicketRes) {
+        this.idTicketRes = idTicketRes;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+}
