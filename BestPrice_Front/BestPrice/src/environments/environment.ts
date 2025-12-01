7668deb275359209abf716d33334541211208e5c
francojara2305 <franco.jara2305@gmail.com>
2025-11-30 23:47:18 -0500

    correccion environment

--- BestPrice_Front/BestPrice/src/environments/environment.ts
+++ BestPrice_Front/BestPrice/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment={
     production:false,
-    base:'https://localhost:8080'
-    //base:'https://bestprice-backend.onrender.com'
+    base:'https://bestprice-backend.onrender.com'
 }
